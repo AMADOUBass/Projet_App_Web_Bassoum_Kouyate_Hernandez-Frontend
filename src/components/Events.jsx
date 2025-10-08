2cf5b5ca25560d5087b483cb7fa43d6c9c81a929
Amadou KOUYATE <amkou002@edu.cegepgarneau.ca>
2025-10-08 13:39:51 -0400

    feat: suppression des evenements

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -88,7 +88,7 @@ export default function Event() {
   return (
     <div className="p-6">
       <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold text-white">📅 Événements</h1>
+        <h2 className="text-2xl font-bold text-white mr-5">📅 Événements</h2>
         <button
           onClick={() => setShowModal(true)}
           className="flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg shadow"
